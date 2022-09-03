97b034546192230732b06b76bbc86fc2e1b73540
richardwzp <wzprichardwzp@gmail.com>
2022-09-03 02:16:11 -0400

    enhanced programming langauge section

--- src/pages/Home/sections/interest/InterestSection.tsx
+++ src/pages/Home/sections/interest/InterestSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './interestSection.css';
-import python from './../../../../ImageResource/python.svg';
-import interestContent from "./interestContent";
+import { regularPlContent, learningPlContent } from "./interestContent";
 
 const InterestBLock = ({name, entry}: any) => {
     return (
@@ -16,11 +15,21 @@ const InterestSection = () => {
 
     return (
         <div style={{display: "flex", flexDirection: "column", width: "100%", paddingLeft: "50px"}}>
-            <div className="titleName">My Interest</div>
+            <div className="titleName">Programming Language </div>
+            <div> The very first introductory computer science course I took at NEU were taught by
+                two awesome professors who come from Programming language background. Since then
+                I have fell in love with it, and even took a compiler course in Spring of 2022.
+                I like to explore the design choices made by languages, and study the impact they have on the
+                code written in them.
+                </div>
             <div style={{display: "flex", flexDirection: "row"}}>
                 <InterestBLock
-                    name="Programming Language"
-                    entry={interestContent}/>
+                    name="languages I regularly use"
+                    entry={regularPlContent}/>
+
+                <InterestBLock
+                    name="language I'm still experimenting with"
+                    entry={learningPlContent}/>
             </div>
         </div>
     );
