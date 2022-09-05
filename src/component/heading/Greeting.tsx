f9f6f2fe4ddfd8ec390fcad3c75d71a404dab609
richardwzp <wzprichardwzp@gmail.com>
2022-09-05 18:45:13 -0400

    modified scaling to be more mobile friendly, added custom media rule for font on mobile

--- src/component/heading/Greeting.tsx
+++ src/component/heading/Greeting.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import "@fontsource/abril-fatface";
 import Typography from "@mui/material/Typography";
-const Greeting = ({children}: any) => {
+const Greeting = ({children, className}: any) => {
 const homeStyle = {
     fontFamily: "Abril Fatface",
     fontSize: 20,
     color: "white",
 
 }
+
+const cname = className === undefined ? "" : className;
    return (
-      <Typography sx={homeStyle}>{children}</Typography>
+      <Typography sx={homeStyle} className={cname}>{children}</Typography>
    )
 }
 
