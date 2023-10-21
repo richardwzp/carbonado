0a8060a300f17d64114bf69afe1adbf73f266beb
richardwzp <wang.zipeng@northeastern.edu>
2023-10-21 16:18:15 -0400

    intro update, add aws section

--- src/pages/Home/sections/intro/IntroSection.tsx
+++ src/pages/Home/sections/intro/IntroSection.tsx
@@ -7,50 +7,50 @@ import greenMe2 from "../../../../ImageResource/greenMe2.png";
 import './introSection.css';
 
 const textElementStyle = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }
 
-const name = <MyName/>
+const name = <MyName />
 
 const textElement = (
-    <div className="textBlockContainer" style={textElementStyle}>
-        <Fade top cascade>
-            <div style={{display: "flex", flexDirection: "column"}}>
-                <div style={{width: "100%"}}>
-                    <Greeting>Hello, this is</Greeting>
-                    {name}
-                </div>
-                <div style={{maxWidth: "450px", lineHeight: 2}}>
-                    I am a rising junior at Northeastern University studying Computer Science with a software concentration.
-                    <br/>
-                    A Programming language enthusiast, avid D&D player/DM, fantasy artist, comic-book fan, and cinephile.
-                </div>
-            </div>
-        </Fade>
-    </div>
+  <div className="textBlockContainer" style={textElementStyle}>
+    <Fade top cascade>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ width: "100%" }}>
+          <Greeting>Hello, this is</Greeting>
+          {name}
+        </div>
+        <div style={{ maxWidth: "450px", lineHeight: 2 }}>
+          I am a rising Senior at Northeastern University studying Computer Science with a software concentration.
+          <br />
+          A Programming language enthusiast, avid D&D player/DM, fantasy artist, comic-book fan, and cinephile.
+        </div>
+      </div>
+    </Fade>
+  </div>
 );
 
 const introPictureStyle = {
-    display: "flex",
-    justifyContent: "center",
-    gap: 20,
-    }
+  display: "flex",
+  justifyContent: "center",
+  gap: 20,
+}
 
-const IntroSection = ({style, className}: any) => {
-return (
+const IntroSection = ({ style, className }: any) => {
+  return (
     <div className={className} style={style}>
-        <div className="introPicture" style={introPictureStyle}>
-            {textElement}
-            <div className={"photoContainer"}>
-                <Zoom>
-                    <img src={greenMe2} alt="me" style={{maxHeight: '600px', border: "0px solid", borderColor: "white"}}/>
-                </Zoom>
-            </div>
+      <div className="introPicture" style={introPictureStyle}>
+        {textElement}
+        <div className={"photoContainer"}>
+          <Zoom>
+            <img src={greenMe2} alt="me" style={{ maxHeight: '600px', border: "0px solid", borderColor: "white" }} />
+          </Zoom>
         </div>
+      </div>
     </div>
-);
+  );
 }
 
 export default IntroSection;
--- src/pages/Home/sections/journey/journeyContent.ts
+++ src/pages/Home/sections/journey/journeyContent.ts
@@ -1,43 +1,54 @@
 
 type JContent = {
-    company: string,
-    title: string,
-    time: string,
-    description: string
+  company: string,
+  title: string,
+  time: string,
+  description: string
 }
 
 const wolterDescription = "For my first Co-op (Cooperative education positions), I work as a member of the Cloud SRE team to " +
-    "maintain and provision critical components in the cloud infrastructure. " +
-    "The goal of our team is to automate the build/release process, " +
-    "deploy domain application with scalability, reliability, and availability. " +
-    "I worked with various cloud provision, orchestration, and automation tools like Terraform, Kubernetes, and Jenkins. " +
-    "It is fascinating how all these different technology integrates with each other " +
-    "to standardize and automate the development process. " +
-    "I enjoyed devOp's unique culture of collaboration, and its deep connection with the open source community. " +
-    "There's so much more to learn, and I'm exited to see what the rest of this " +
-    "experience will bring me!"
+  "maintain and provision critical components in the cloud infrastructure. " +
+  "The goal of our team is to automate the build/release process, " +
+  "deploy domain application with scalability, reliability, and availability. " +
+  "I worked with various cloud provision, orchestration, and automation tools like Terraform, Kubernetes, and Jenkins. " +
+  "It is fascinating how all these different technology integrates with each other " +
+  "to standardize and automate the development process. " +
+  "I enjoyed devOp's unique culture of collaboration, and its deep connection with the open source community. " +
+  "There's so much more to learn, and I'm exited to see what the rest of this " +
+  "experience will bring me!"
 
 const taDescription = "I was a TA for Object-Oriented Design in fall 2021, Fundamental of Computer Science in spring 2022, " +
-    "and then Objected-Oriented Design again in summer 2022. Being a teaching assistant gave me a unique perspective to " +
-    "view the course from an instructor point of view. I frequently hold office hours and review sessions to help students " +
-    " clear up important conceptual misunderstanding, and provide one-on-one help. Objected Oriented Design, as the name " +
-    "suggests, focuses on design principles and a test-driven development approach. Being a TA is not only a way for me to" +
-    "give back to the community, but it also improved my reading comprehension for foreign code, and my ability to articulate " +
-    "abstract design pattern and design ideas. It is a valuable experience that will help me in the professional world. "
+  "and then Objected-Oriented Design again in summer 2022. Being a teaching assistant gave me a unique perspective to " +
+  "view the course from an instructor point of view. I frequently hold office hours and review sessions to help students " +
+  " clear up important conceptual misunderstanding, and provide one-on-one help. Objected Oriented Design, as the name " +
+  "suggests, focuses on design principles and a test-driven development approach. Being a TA is not only a way for me to" +
+  "give back to the community, but it also improved my reading comprehension for foreign code, and my ability to articulate " +
+  "abstract design pattern and design ideas. It is a valuable experience that will help me in the professional world. "
+
+const awsDescription = "I was an intern on the Amazon Connect Onboarding team. I designed and implemented a customer requested UI feature in reactJS. " +
+  "Working on this project taught me a lot about collborating with teamates in a corporate environement. " +
+  "It also made me a much more effective frontend engineer, and " +
+  "I look forward to exploring more about frontend technology in the future!"
 
 const journeyContent: JContent[] = [
-    {
-        company: "Wolterskluwer",
-        title: "DevOp Engineering Intern",
-        time: "July 2022 - now",
-        description: wolterDescription,
-    },
-{
-        company: "Northeastern University",
-        title: "Teaching Assistant",
-        time: "Sept 2021 - June 2022",
-        description: taDescription,
-    }
+  {
+    company: "Wolterskluwer",
+    title: "DevOp Engineering Intern",
+    time: "July 2022 - now",
+    description: wolterDescription,
+  },
+  {
+    company: "Northeastern University",
+    title: "Teaching Assistant",
+    time: "Sept 2021 - June 2022",
+    description: taDescription,
+  },
+  {
+    company: "AWS",
+    title: "Software engineering Intern",
+    time: "May 2023 - August 2023",
+    description: awsDescription,
+  }
 ]
 
 
